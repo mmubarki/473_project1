7da9497390fd8248e898d40ee6cf3440932285fa
Mussa Mubarki <mmubarki@gmail.com>
2015-03-08 23:49:56 -0700

    create callAPI function and add README.md
    create callAPI function that will be for all API calls
    add README.md

--- API/javascripts/app.js
+++ API/javascripts/app.js
@@ -1,6 +1,8 @@
 
-var poster =  function(){
-
+ /*
+    this is main function
+  */
+var main =  function(){
    $('#movie_name').focus(function(){
       var full = $("#results").has("img").length ? true : false;
       if(full == false){
@@ -8,6 +10,29 @@ var poster =  function(){
       }
    });
 
+   /*
+      callAPI is used to call any API
+   */
+   function callAPI(APIUrl,successCall,errorCall,dataType,callType,dataFormat){
+      $.ajax({
+        url:APIUrl,
+        data: {
+          format: dataFormat
+        },
+        error : function () {
+          errorCall();
+        },
+        dataType: dataType,
+        success: function (data) {
+          successCall(data);
+        },
+        type: callType
+      });
+   }
+
+   /*
+      this is the enent function will be call when click search
+   */
    var getPoster = function(){
 
         var film = $('#movie_name').val();
@@ -19,30 +44,33 @@ var poster =  function(){
          } else {
 
             $('#results').html("<h3>please wait</h3>");
-            $.ajax({
-              url: 'http://api.themoviedb.org/3/search/movie?api_key=ae22cfb91b3d2c0a8179d99a29831489&query=' + film ,
-              data: {
-                 format: 'json'
-              },
-              error: function() {
-                 $('#results').html('<h3>error in ajax call</h3><img id="poster" src="" />');
-              },
-              dataType: 'jsonp',
-              success: function(data) {
+            var url = 'http://api.themoviedb.org/3/search/movie?api_key=ae22cfb91b3d2c0a8179d99a29831489&query=' + film;
+            
+            var successCall = function(data) {
+                console.log("inside successCall");
+                console.log(data);
                 if (data.total_results > 0){
                   $('#results').html('<h3 class="loading">We found the poster</h3><img id="poster" src="http://image.tmdb.org//t//p//w500' + data.results[0].poster_path+ '"/>');
                 } else {
                   $('#results').html('<h3 class="loading">We do not find what you search</h3><img id="poster" src="" />');
                 }
-              },
-              type: 'GET'
-           });
+              }
+            var errorCall =function() {
+                 $('#results').html('<h3>error in ajax call</h3><img id="poster" src="" />');
+              };
+            var dataFormat='json',
+                dataType='jsonp',
+                callType='GET';
+            callAPI(url,successCall,errorCall,dataType,callType,dataFormat);
           }
 
         return false;
    }
 
+
+  // set button event function
    $('#search').click(getPoster);
+   // we can search by click enter afetr type
    $('#movie_name').keyup(function(event){
        if(event.keyCode == 13){
            getPoster();
@@ -50,4 +78,4 @@ var poster =  function(){
    });
 
 };
-$(document).ready(poster);
+$(document).ready(main);
